d2c4f3a6d3511805e3b0256ecc38756fbb231ad5
TepsicMarko <73076336+TepsicMarko@users.noreply.github.com>
2021-11-29 13:46:17 +0100

    refactor

--- main.js
+++ main.js
@@ -1,29 +1,119 @@
-let popisUcenika = [["Bago",i=0], ["Blazevic",i=0], ["Drobac",i=0], ["Gavic",i=0], ["Habunek",i=0], ["Impric",i=0], ["Ivankovic",i=0],
-                    ["Jaki",i=0], ["Jabmresic",i=0], ["Jurkovic",i=0], ["Krot",i=0], ["Kupsjak",i=0], ["Lozic",i=0], ["Mazic",i=0], ["Obradovic",i=0], 
-                    ["Pancic",i=0],["Pilipovic",i=0], ["Raketic",i=0], ["Rus",i=0], ["Senjug",i=0], ["stricak",i=0], ["Tepsic",i=0], ["Turk",i=0]];
-const newLine=document.createElement('br');
-let button=document.querySelector("button").addEventListener("click", function(){
-    let container=document.querySelector("#container");
-    container.innerHTML="";
-    let brojSimulacija= prompt("Koliko puta želiš simulirati slučajni odabir?")
-    for(var b=0; b<brojSimulacija; b++)
-        {
-            let randomNumber= Math.floor(Math.random()*popisUcenika.length);
-            popisUcenika[randomNumber][1]=popisUcenika[randomNumber][1]+1;
-        }
-    let popisPoVel= popisUcenika.sort((a,b)=> a[1]-b[1]);
-    let c=0;
-        popisPoVel.forEach(element => 
-            { 
-                if(popisPoVel[c][1]>0){
-                    let p=document.createElement("p")
-                    p.innerText=`${popisPoVel[c][0]} je izabran ${popisPoVel[c][1]} puta`;//Bago je izabran 2 puta 
-                    container.appendChild(p);
-                }
-                c++;
-            });
-    popisPoVel=[];
-    popisUcenika=[["Bago",i=0], ["Blazevic",i=0], ["Drobac",i=0], ["Gavic",i=0], ["Habunek",i=0], ["Impric",i=0], ["Ivankovic",i=0],
-    ["Jaki",i=0], ["Jabmresic",i=0], ["Jurkovic",i=0], ["Krot",i=0], ["Kupsjak",i=0], ["Lozic",i=0], ["Mazic",i=0], ["Obradovic",i=0], 
-    ["Pancic",i=0],["Pilipovic",i=0], ["Raketic",i=0], ["Rus",i=0], ["Senjug",i=0], ["stricak",i=0], ["Tepsic",i=0], ["Turk",i=0]];
-})
+const ucenici = [
+  {
+    ime: 'Bago',
+    izvucen: 0
+  },
+  {
+    ime: 'Blaz',
+    izvucen: 0
+  },
+  {
+    ime: 'Drobac',
+    izvucen: 0
+  },
+  {
+    ime: 'Gavic',
+    izvucen: 0
+  },
+  {
+    ime: 'Robi',
+    izvucen: 0
+  },
+  {
+    ime: 'Impra',
+    izvucen: 0
+  },
+  {
+    ime: 'Bruno',
+    izvucen: 0
+  },
+  {
+    ime: 'Jaki',
+    izvucen: 0
+  },
+  {
+    ime: 'Jambre',
+    izvucen: 0
+  },
+  {
+    ime: 'Jurki',
+    izvucen: 0
+  },
+  {
+    ime: 'Krot',
+    izvucen: 0
+  },
+  {
+    ime: 'Kupsjak',
+    izvucen: 0
+  },
+  {
+    ime: 'Loza',
+    izvucen: 0
+  },
+  {
+    ime: 'Znac',
+    izvucen: 0
+  },
+  {
+    ime: 'Obra',
+    izvucen: 0
+  },
+  {
+    ime: 'Panix',
+    izvucen: 0
+  },
+  {
+    ime: 'Pilpi',
+    izvucen: 0
+  },
+  {
+    ime: 'Raketic',
+    izvucen: 0
+  },
+  {
+    ime: 'Rus',
+    izvucen: 0
+  },
+  {
+    ime: 'Strix',
+    izvucen: 0
+  },
+  {
+    ime: 'Senjug',
+    izvucen: 0
+  },
+  {
+    ime: 'Tepsic',
+    izvucen: 0
+  },
+  {
+    ime: 'Turk',
+    izvucen: 0
+  },
+]
+
+const btn = document.getElementsByTagName('button')[0]
+const container = document.querySelector("#container");
+
+const slucajniOdabir = () => {
+  container.innerHTML = "";
+  const zrtve = prompt('koliko zrtava os')
+  const temp = JSON.parse(JSON.stringify(ucenici))
+
+  Array.from({length: parseInt(zrtve)}).map(izvlacenje => {
+    const rnd = Math.floor(Math.random() * 23)
+    temp[rnd].izvucen += 1
+  })
+
+  const zrtvovani = temp.filter(ucenik => ucenik.izvucen > 0).sort((a, b) => a.izvucen - b.izvucen)
+
+  zrtvovani.forEach(zrtva => {
+    const p = document.createElement('p')
+    p.textContent = zrtva.ime + ' je izvucen ' + zrtva.izvucen + ' puta'
+    container.appendChild(p)
+  })
+
+}
+
+btn.addEventListener('click', slucajniOdabir)
